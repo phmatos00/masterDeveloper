69a647f9493978796d8c1ae50aff8a7d411950f0
Paulo sérgio <pauser2200@gmail.com>
2021-02-09 19:19:16 -0300

    .

---
+++ teste fibonacci/src/main/java/teste/fibonacci/Fibonacci.java
@@ -0,0 +1,22 @@
+package teste.fibonacci;
+
+public class Fibonacci {
+
+	public static void main(String[] args) {
+		
+		int vezes = 10;
+		
+		for (int cont = 0, i = 0, j = 1; cont < vezes; cont++ ) {
+            System.out.print(i + " ");
+            
+            i = i + j;
+            j = i - j;
+			
+	
+	
+		}
+		
+
+	}
+
+}
